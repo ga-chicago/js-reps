20bba2f46b2e0ad53d9269ad55855927a17b5b14
Samat Baltin <somych1@gmail.com>
2018-03-16 01:10:08 -0500

    round 3

--- js/app.js
+++ js/app.js
@@ -29,3 +29,20 @@ const transmogrifier = function(num1, num2, num3){
 transmogrifier(5, 4, 3);
 transmogrifier(12, 12, 6);
 transmogrifier(42, 13, 7);
+
+
+
+
+
+// ROUND 3
+
+const toonify = function(accent, sentence){
+	if (accent === "daffy") {
+		return sentence.replace(/s/g, "th")
+	} else if (accent === "elmer") {
+		return sentence.replace(/r/g, "w")
+	} else {
+		return sentence
+	}
+}
+toonify("daffy", "so you smell like sausage")
