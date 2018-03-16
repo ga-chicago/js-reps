498291aeaf5718cb7a01d05706b759a5e608217c
Samat Baltin <somych1@gmail.com>
2018-03-16 01:01:07 -0500

    round 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+
+
+
+
+// ROUND 1
+
 const words = ["hello", "what", "is", "up", "dude"];
 
 const length = function(arrayOfStrings){
@@ -9,3 +15,17 @@ const length = function(arrayOfStrings){
 }
 
 length(words);
+
+
+
+
+
+
+// ROUND 2
+
+const transmogrifier = function(num1, num2, num3){
+	return Math.pow((num1 * num2), num3);
+}
+transmogrifier(5, 4, 3);
+transmogrifier(12, 12, 6);
+transmogrifier(42, 13, 7);
